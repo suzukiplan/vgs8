17f9335471572ada6679fa2d6aa51d9300219f05
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-24 17:09:50 +0900

    1文字しか描画しない

--- examples/hello/hello.asm
+++ examples/hello/hello.asm
@@ -13,7 +13,7 @@ draw_loop:
     sta $63C7, x ; write on (7 + x, 15) = ($007 + x, $3C0 = 15 * 64)
     inx
     cpx #18 ; text length
-    bne draw_loop
+    beq draw_loop
 
 mainloop:
     lda $5BFF ; Wait for VSYNC
