07a4f698cf31f2854f732200e70a75b3a86b58f4
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-24 19:18:43 +0900

    Revert "1文字しか描画しない"
    This reverts commit 17f9335471572ada6679fa2d6aa51d9300219f05.

--- examples/hello/hello.asm
+++ examples/hello/hello.asm
@@ -13,7 +13,7 @@ draw_loop:
     sta $63C7, x ; write on (7 + x, 15) = ($007 + x, $3C0 = 15 * 64)
     inx
     cpx #18 ; text length
-    beq draw_loop
+    bne draw_loop
 
 mainloop:
     lda $5BFF ; Wait for VSYNC
