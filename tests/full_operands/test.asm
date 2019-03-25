2985f53a86d4c5a89e11418b2b4108447e440437
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-25 22:49:57 +0900

    update test

--- tests/full_operands/test.asm
+++ tests/full_operands/test.asm
@@ -2,12 +2,46 @@
 .autoimport on
 
 .segment "STARTUP"
-    LDA #$01
-    STA $01
-    LDA #$02
-    STA $02
-    AND #$00
+    ; テストしやすいようにゼロページの値をアドレスと同値にしておく
+    LDX #$00
+LOOP1:
+    TXA
+    STA $00, X
+    INX
+    BNE LOOP1
+
+    ; 特に意味は無いがゼロページを全部ロード
+    LDY #$00
+LOOP2:
+    LDA $00, Y
+    INY
+    BNE LOOP2
+
+    ; ついでにDEX DEYをテストしておく
+    DEX
+    DEY
+
+    ; LDX/LDYのゼロページ参照をテスト
+    LDX $05
+    LDY $05
+    LDX $05, Y
+    LDY $05, X
+
     LDA $00
-    LDX #$01
-    LDA $00, X
-    LDA $01, X
+    LDA $01
+    LDA $02
+    LDA $03
+
+    ; indirectでのロード・ストア（私はFCゲーム開発で使ったことが無いけど）
+    LDA ($01, X)
+    LDA ($01), Y
+
+    ; absoluteなload/storeパターンを網羅しておく
+    LDA $1000
+    LDA $1000, X
+    LDA $1000, Y
+    LDX $2000
+    LDX $2000, Y
+    LDY $3000
+    LDY $3000, X
+    BRK
