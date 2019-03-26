6724f28906eafc30c24a52dd669e42ca57f3e9a8
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-26 22:49:54 +0900

    何故かHello, World!にエイリアンを登場させる

--- examples/hello_sp/hello.asm
+++ examples/hello_sp/hello.asm
@@ -8,6 +8,8 @@
 .segment "STARTUP"
     jsr initialize
 mainloop:
+    jsr add_new_enemy
+    jsr move_enemies
     jsr move_player
     jsr move_player_shots
     jsr scroll_bg
@@ -200,6 +202,132 @@ move_player_shot_1:
     rts
 
 ;-------------------------------------------------------------------------------
+; Add new enemy
+;-------------------------------------------------------------------------------
+add_new_enemy:
+    ; check timer (enemy emergence will per 16 frames)
+    lda v_enemyT
+    clc
+    adc #1
+    sta v_enemyT
+    and #$0F
+    beq add_new_enemy_1
+    rts
+add_new_enemy_1:
+    ; check still enemy is exist at the current index
+    ldx v_enemyI
+    lda v_enemy + 0, x
+    beq add_new_enemy_2
+    rts
+add_new_enemy_2:
+    ; create an enemy
+    inc v_enemy + 0, x
+    lda #$01 ; type 1
+    sta v_enemy + 1, x
+    lda #$00 ; clear v1, v2
+    sta v_enemy + 2, x
+    sta v_enemy + 3, x
+    ; set x (random: 0 ~ 224)
+    jsr get_rand_to_a
+    cmp #224
+    bcc add_new_enemy_3
+    sec
+    sbc #32
+add_new_enemy_3:
+    clc
+    adc #8
+    sta sp_enemy + 0, x
+    lda #0
+    sta sp_enemy + 1, x
+    lda #$12
+    sta sp_enemy + 2, x
+    lda #%00000001
+    sta sp_enemy + 3, x
+    ; add 4 the enemy index
+    txa
+    clc
+    adc #4
+    and #63
+    sta v_enemyI
+    rts
+
+;-------------------------------------------------------------------------------
+; Move enemies
+;-------------------------------------------------------------------------------
+move_enemies:
+    ldx #0
+move_enemies_1:
+    ; check flag
+    lda v_enemy + 0, x
+    bne move_enemies_2
+    jmp move_enemies_next
+move_enemies_2:
+    ; check type
+    lda v_enemy + 1, x
+    cmp #1
+    bne move_enemies_3
+    jsr move_enemy_type1
+    jmp move_enemies_next
+move_enemies_3:
+    cmp #2
+    bne move_enemies_4
+    jsr move_enemy_type2
+    jmp move_enemies_next
+move_enemies_4:
+    cmp #3
+    bne move_enemies_5
+    jsr move_enemy_type2
+    jmp move_enemies_next
+move_enemies_5:
+    brk ; BUG: unknown enemy type
+move_enemies_next:
+    ; add index
+    txa
+    clc
+    adc #4
+    and #63
+    beq move_enemies_end
+    tax
+    jmp move_enemies_1 ; check the next enemy
+move_enemies_end:
+    rts
+
+; Move enemy (type1)
+move_enemy_type1:
+    lda sp_enemy + 1, x
+    clc
+    adc #2
+    sta sp_enemy + 1, x
+    cmp #248
+    bcc move_enemy_type1_1
+    jsr remove_enemy
+move_enemy_type1_1:
+    lda v_enemy + 2, x
+    clc
+    adc #1
+    sta v_enemy + 2, x
+    and #$07
+    lsr
+    lsr
+    asl
+    clc
+    adc #$12
+    sta sp_enemy + 2, x
+    rts
+
+move_enemy_type2:
+    rts
+
+move_enemy_type3:
+    rts
+
+remove_enemy:
+    lda #0
+    sta v_enemy + 0, x ; clear flag
+    sta sp_enemy + 2, x ; clear sprite
+    rts
+
+;-------------------------------------------------------------------------------
 ; Scroll BG
 ;-------------------------------------------------------------------------------
 scroll_bg:
@@ -210,10 +338,43 @@ scroll_bg:
     rts
 
 ;-------------------------------------------------------------------------------
+; Get random value to a (no change all registers)
+; NOTE: use $00 for work area
+;-------------------------------------------------------------------------------
+get_rand_to_a:
+    php
+    stx $00
+    ldx v_randI
+    lda rand_table, x
+    inx
+    stx v_randI
+    ldx $00
+    plp
+    rts
+
+;-------------------------------------------------------------------------------
 ; String literal definition
 ;-------------------------------------------------------------------------------
 string_hello_world:
     .byte "Hello, VGS8 World!"
+
+rand_table:; 乱数テーブル
+    .byte   $72,$DD,$03,$89,$C9,$86,$DB,$30,$8E,$4F,$DC,$99,$67,$54,$13,$4C
+    .byte   $A3,$CA,$D8,$28,$50,$0F,$8B,$87,$6B,$B9,$10,$CF,$EC,$40,$FD,$B6
+    .byte   $F3,$AF,$70,$56,$74,$CC,$47,$60,$B4,$0C,$80,$16,$D7,$79,$61,$BC
+    .byte   $C8,$11,$B0,$A8,$FE,$5A,$B5,$62,$A6,$6E,$6D,$77,$CE,$32,$4D,$55
+    .byte   $9E,$09,$21,$83,$9A,$F8,$E5,$BF,$8A,$BA,$2E,$4B,$EB,$9F,$D3,$36
+    .byte   $CB,$07,$44,$EE,$E4,$9D,$73,$5F,$F4,$00,$1E,$78,$3A,$EA,$D1,$BD
+    .byte   $A0,$D6,$C0,$3D,$66,$19,$FB,$92,$C7,$53,$7C,$D4,$98,$E7,$C4,$AC
+    .byte   $7F,$7A,$93,$B8,$0A,$8C,$A2,$06,$E8,$A5,$29,$88,$BE,$CD,$0E,$DA
+    .byte   $5B,$5C,$A9,$02,$7E,$EF,$ED,$D9,$E6,$F1,$3E,$05,$45,$8D,$F6,$23
+    .byte   $E3,$FC,$4A,$D2,$91,$F9,$20,$C3,$04,$68,$18,$49,$2F,$94,$2C,$2B
+    .byte   $01,$2D,$27,$1D,$6A,$9B,$12,$A1,$A4,$DF,$76,$6C,$64,$69,$1C,$C6
+    .byte   $08,$AD,$3F,$96,$65,$C2,$38,$5D,$7B,$E2,$97,$41,$1F,$E0,$9C,$B3
+    .byte   $48,$84,$51,$8F,$22,$5E,$46,$57,$31,$37,$4E,$0D,$58,$AE,$AB,$7D
+    .byte   $B2,$FF,$AA,$D0,$59,$3C,$35,$26,$34,$24,$A7,$1A,$52,$15,$95,$C1
+    .byte   $17,$71,$BB,$25,$82,$75,$3B,$E1,$14,$C5,$0B,$F5,$6F,$E9,$DE,$F2
+    .byte   $42,$33,$43,$B1,$F0,$90,$B7,$39,$85,$D5,$81,$1B,$63,$2A,$FA,$F7
 
 ;-------------------------------------------------------------------------------
 ; WRAM (variable labels)
@@ -223,6 +384,25 @@ v_scroll:   .byte $00
 v_shotW:    .byte $00 ; wait fire flag
 v_shotI:    .byte $00 ; index of the player shot
 v_shotF:    .byte $00, $00, $00, $00, $00, $00, $00, $00 ; flags of the player shot
+v_randI:    .byte $00 ; random index
+v_enemyT:   .byte $00 ; enemy timer
+v_enemyI:   .byte $00 ; enemy index
+v_enemy:    .byte $00, $00, $00, $00    ; enemy[0] vars (flag, type, v1, v2)
+            .byte $00, $00, $00, $00    ; enemy[1] vars
+            .byte $00, $00, $00, $00    ; enemy[2] vars
+            .byte $00, $00, $00, $00    ; enemy[3] vars
+            .byte $00, $00, $00, $00    ; enemy[4] vars
+            .byte $00, $00, $00, $00    ; enemy[5] vars
+            .byte $00, $00, $00, $00    ; enemy[6] vars
+            .byte $00, $00, $00, $00    ; enemy[7] vars
+            .byte $00, $00, $00, $00    ; enemy[8] vars
+            .byte $00, $00, $00, $00    ; enemy[9] vars
+            .byte $00, $00, $00, $00    ; enemy[10] vars
+            .byte $00, $00, $00, $00    ; enemy[11] vars
+            .byte $00, $00, $00, $00    ; enemy[12] vars
+            .byte $00, $00, $00, $00    ; enemy[13] vars
+            .byte $00, $00, $00, $00    ; enemy[14] vars
+            .byte $00, $00, $00, $00    ; enemy[15] vars
 
 ;-------------------------------------------------------------------------------
 ; Sprite OAM labels
@@ -237,3 +417,19 @@ sp_shot:    .byte $00, $00, $00, $00    ; shot[0]
             .byte $00, $00, $00, $00    ; shot[5]
             .byte $00, $00, $00, $00    ; shot[6]
             .byte $00, $00, $00, $00    ; shot[7]
+sp_enemy:   .byte $00, $00, $00, $00    ; enemy[0]
+            .byte $00, $00, $00, $00    ; enemy[1]
+            .byte $00, $00, $00, $00    ; enemy[2]
+            .byte $00, $00, $00, $00    ; enemy[3]
+            .byte $00, $00, $00, $00    ; enemy[4]
+            .byte $00, $00, $00, $00    ; enemy[5]
+            .byte $00, $00, $00, $00    ; enemy[6]
+            .byte $00, $00, $00, $00    ; enemy[7]
+            .byte $00, $00, $00, $00    ; enemy[8]
+            .byte $00, $00, $00, $00    ; enemy[9]
+            .byte $00, $00, $00, $00    ; enemy[10]
+            .byte $00, $00, $00, $00    ; enemy[11]
+            .byte $00, $00, $00, $00    ; enemy[12]
+            .byte $00, $00, $00, $00    ; enemy[13]
+            .byte $00, $00, $00, $00    ; enemy[14]
+            .byte $00, $00, $00, $00    ; enemy[15]
