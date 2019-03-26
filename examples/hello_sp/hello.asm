e101ec5e5d37c643acb432ff52c1efc66848c3f6
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-26 16:41:36 +0900

    何故かHello, World!でショットを発射できるようにしてみる

--- examples/hello_sp/hello.asm
+++ examples/hello_sp/hello.asm
@@ -8,6 +8,7 @@
     jsr initialize
 mainloop:
     jsr move_player
+    jsr move_player_shots
     jsr scroll_bg
     lda $5BFF ; Wait for VSYNC
     jmp mainloop
@@ -99,6 +100,102 @@ move_player_3:
     iny
     sty sp_playerX
 move_player_4:
+    txa
+    and #%00001100
+    beq move_player_5
+    lda v_shotW
+    bne move_player_6
+    jsr fire_player_shot
+    rts
+move_player_5:
+    lda v_shotW
+    beq move_player_6
+    dec v_shotW ; decrement wait fire flag
+    rts
+move_player_6:
+    rts
+
+;-------------------------------------------------------------------------------
+; Fire player shot
+;-------------------------------------------------------------------------------
+fire_player_shot:
+    ; set 1 to v_shotF[v_shotI]
+    ldx v_shotI
+    lda #1
+    sta v_shotF, x
+    ; increment v_shotI (and loop 0 ~ 7)
+    inc v_shotI
+    lda v_shotI
+    and #$07
+    sta v_shotI
+    ; calcurate index of sprite (x4) to x
+    txa
+    asl
+    asl
+    tax
+    ; set initial X
+    lda sp_playerX
+    clc
+    adc #4
+    sta sp_shot, x
+    ; set initial Y
+    lda sp_playerY
+    sec
+    sbc #8
+    sta sp_shot + 1, x
+    ; set pattern
+    lda #$02
+    sta sp_shot + 2, x
+    ; set flags
+    lda #$00
+    sta sp_shot + 3, x
+    ; set wait fire flag
+    lda #4
+    sta v_shotW
+    rts
+
+;-------------------------------------------------------------------------------
+; Move player shots
+;-------------------------------------------------------------------------------
+move_player_shots:
+    ldx #0
+move_player_shots_1:
+    lda v_shotF, x
+    beq move_player_shots_2 ; branch if flag is unset
+    txa
+    pha
+    asl
+    asl
+    tax
+    jsr move_player_shot
+    pla
+    tax
+move_player_shots_2:
+    inx
+    cpx #8
+    bne move_player_shots_1
+    rts
+
+; move a shot (x is index of the shot)
+move_player_shot:
+    ; move up 4px
+    lda sp_shot + 1, x
+    sec
+    sbc #4
+    sta sp_shot + 1, x
+    bcs move_player_shot_1
+    rts
+move_player_shot_1:
+    ; hide sprite
+    lda #0
+    sta sp_shot + 2, x
+    ; clear flag
+    txa
+    lsr
+    lsr
+    tax
+    lda #0
+    sta v_shotF, x
     rts
 
 ;-------------------------------------------------------------------------------
@@ -122,12 +219,23 @@ string_hello_world:
 ;-------------------------------------------------------------------------------
 .org $0200
 v_scroll:   .byte $00
+v_shotW:    .byte $00 ; wait fire flag
+v_shotI:    .byte $00 ; index of the player shot
+v_shotF:    .byte $00, $00, $00, $00, $00, $00, $00, $00 ; flags of the player shot
 
 ;-------------------------------------------------------------------------------
 ; Sprite OAM labels (OAM: x, y, pattern, falgs)
 ;-------------------------------------------------------------------------------
 .org $5000 
-sp_playerX: .byte $00
+sp_playerX: .byte $00                   ; player
 sp_playerY: .byte $00
 sp_playerP: .byte $00
 sp_playerF: .byte $00
+sp_shot:    .byte $00, $00, $00, $00    ; shot[0]
+            .byte $00, $00, $00, $00    ; shot[1]
+            .byte $00, $00, $00, $00    ; shot[2]
+            .byte $00, $00, $00, $00    ; shot[3]
+            .byte $00, $00, $00, $00    ; shot[4]
+            .byte $00, $00, $00, $00    ; shot[5]
+            .byte $00, $00, $00, $00    ; shot[6]
+            .byte $00, $00, $00, $00    ; shot[7]
