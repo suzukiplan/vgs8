97e34a68f16265034be39eadbeddd5505ea4da34
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-28 01:05:58 +0900

    音声の再生に対応

--- hal/mac/EmuBoard/EmuBoard.cpp
+++ hal/mac/EmuBoard/EmuBoard.cpp
@@ -9,14 +9,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mutex>
 #include "EmuBoard.h"
 #include "vgs8.h"
+#include "vgsspu_al.h"
 
 /**
  * この内容を書き換えればそれが画面に表示される
  * ※RGB565（16bit color）形式
  */
 unsigned short emu_vram[VRAM_WIDTH * VRAM_HEIGHT];
+
+static void* spu;
+static std::mutex sound_lock;
+static unsigned short sound_buffer[65536];
+static unsigned short sound_cursor;
 
 int emu_key_up;
 int emu_key_down;
@@ -32,6 +39,15 @@ unsigned char emu_touchY;
 
 static VGS8::VirtualMachine* vm;
 
+
+void sound_proc(void* buffer, size_t size)
+{
+    std::unique_lock<std::mutex> lock(sound_lock);
+    memcpy(buffer, sound_buffer, size);
+    if (size <= sound_cursor) sound_cursor -= size;
+    else sound_cursor = 0;
+}
+
 /**
  * 起動時に1回だけ呼び出される
  */
@@ -39,6 +55,7 @@ void emu_init(void* rom, size_t size)
 {
     puts("emu_init");
     if (vm) return;
+    spu = vgsspu_start2(22050, 16, 1, 4410, sound_proc);
     vm = new VGS8::VirtualMachine(rom, size);
 }
 
@@ -56,6 +73,13 @@ void emu_vsync()
     vm->tick();
     size_t size;
     memcpy(emu_vram, vm->getDisplay565(&size), sizeof(emu_vram));
+    size_t pcmSize;
+    void* pcm = vm->getPCM(&pcmSize);
+    std::unique_lock<std::mutex> lock(sound_lock);
+    if (pcmSize + sound_cursor < 65536) {
+        memcpy(&sound_buffer[sound_cursor], pcm, pcmSize);
+        sound_cursor += pcmSize / 2;
+    }
 }
 
 /**
@@ -68,4 +92,5 @@ void emu_destroy()
     if (!vm) return;
     delete vm;
     vm = NULL;
+    vgsspu_end(spu);
 }
