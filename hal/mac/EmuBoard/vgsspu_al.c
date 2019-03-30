c2360d8f781d9b27cff8aeda08db4be0b55c864b
suzukiplan <suzukiplan.ys@gmail.com>
2019-03-30 17:17:45 +0900

    バッファリング間隔を小さくして効果音賽銭遅延を少なくする

--- hal/mac/EmuBoard/vgsspu_al.c
+++ hal/mac/EmuBoard/vgsspu_al.c
@@ -48,7 +48,7 @@ static void term_al(struct AL* al);
 
 void* vgsspu_start(void (*callback)(void* buffer, size_t size))
 {
-    return vgsspu_start2(22050, 16, 1, 5880, callback);
+    return vgsspu_start2(22050, 16, 1, 1470, callback);
 }
 
 void* vgsspu_start2(int sampling, int bit, int ch, size_t size, void (*callback)(void* buffer, size_t size))
