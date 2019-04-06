bc2743daeee9c83a53657a5fb5cffa072fb7ac0a
suzukiplan <suzukiplan.ys@gmail.com>
2019-04-06 12:55:06 +0900

    keep view size to the NSUserDefaults

--- hal/mac/EmuBoard/ViewController.m
+++ hal/mac/EmuBoard/ViewController.m
@@ -14,6 +14,8 @@
 @interface ViewController() <NSWindowDelegate, VideoViewDelegate>
 @property (nonatomic) VideoView* video;
 @property (nonatomic) BOOL isFullScreen;
+@property (nonatomic) CGFloat currentWindowWidth;
+@property (nonatomic) CGFloat currentWindowHeight;
 @end
 
 @implementation ViewController
@@ -23,7 +25,7 @@
     NSString* romFile = [[NSBundle mainBundle] pathForResource:@"init" ofType:@"rom"];
     NSData* rom = [NSData dataWithContentsOfFile:romFile];
     emu_init((void*)rom.bytes, rom.length);
-    self.view.frame = CGRectMake(0, 0, VRAM_VIEW_WIDTH * 1.5, VRAM_VIEW_HEIGHT * 1.5);
+    [self _setupDefaultWindowSize];
     CALayer *layer = [CALayer layer];
     [layer setBackgroundColor:CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)];
     [self.view setWantsLayer:YES];
@@ -34,6 +36,19 @@
     [self.view.window makeFirstResponder:_video];
 }
 
+- (void)_setupDefaultWindowSize
+{
+    NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+    CGFloat defaultViewWidth = [(NSNumber*)[defaults valueForKey:@"defaultViewWidth"] floatValue];
+    if (!defaultViewWidth) defaultViewWidth = VRAM_VIEW_WIDTH * 1.5;
+    CGFloat defaultViewHeight = [(NSNumber*)[defaults valueForKey:@"defaultViewHeight"] floatValue];
+    if (!defaultViewHeight) defaultViewHeight = VRAM_VIEW_HEIGHT * 1.5;
+    _currentWindowWidth = defaultViewWidth;
+    _currentWindowHeight = defaultViewHeight;
+    [self.view.window setContentSize:NSMakeSize(defaultViewWidth, defaultViewHeight)];
+    self.view.frame = CGRectMake(0, 0, defaultViewWidth, defaultViewHeight);
+}
+
 - (void)viewWillAppear
 {
     self.view.window.delegate = self;
@@ -42,6 +57,14 @@
 - (void)windowDidResize:(NSNotification *)notification
 {
     _video.frame = [self calcVramRect];
+    if (_isFullScreen) return;
+    if (_currentWindowWidth != self.view.frame.size.width || _currentWindowHeight != self.view.frame.size.height) {
+        _currentWindowWidth = self.view.frame.size.width;
+        _currentWindowHeight = self.view.frame.size.height;
+        NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+        [defaults setValue:@(_currentWindowWidth) forKey:@"defaultViewWidth"];
+        [defaults setValue:@(_currentWindowHeight) forKey:@"defaultViewHeight"];
+    }
 }
 
 - (NSRect)calcVramRect
@@ -118,7 +141,7 @@
     [self.view.window setContentSize:NSMakeSize(VRAM_VIEW_WIDTH * 4, VRAM_VIEW_HEIGHT * 4)];
 }
 
-- (void)windowDidEnterFullScreen:(NSNotification *)notification
+- (void)windowWillEnterFullScreen:(NSNotification *)notification
 {
     _isFullScreen = YES;
 }
